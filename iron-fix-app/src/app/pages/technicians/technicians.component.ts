9f1cb9a8ba7c4927a98819cd882a2c72f4953a77
pholcast25 <pcastaneda335@soyudemedellin.edu.co>
2025-04-28 15:38:53 -0500

    Funcionamiento inicial tecnicos

--- iron-fix-app/src/app/pages/technicians/technicians.component.ts
+++ iron-fix-app/src/app/pages/technicians/technicians.component.ts
@@ -1,11 +1,84 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { TechnicianService } from '../../shared/services/technician.service';
+import { User } from '../../shared/interfaces/user.interface';
 
 @Component({
   selector: 'app-technicians',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './technicians.component.html',
   styleUrl: './technicians.component.css'
 })
-export class TechniciansComponent {
+export class TechniciansComponent implements OnInit  {
+  modalVisible = false;
+  isEditing = false;
+  technicians = signal<User[]>([]);
+  
+  fb = inject(FormBuilder);
+  technicianService = inject(TechnicianService);  // Inyectar el servicio
 
+  originalUsername: string | null = null;
+
+
+  technicianForm = this.fb.group({
+    username: ['', Validators.required],
+    email: ['', [Validators.required]],
+    role: ['', [Validators.required]]
+  });
+
+  ngOnInit() {
+    this.loadTechnicians();
+  }
+
+  loadTechnicians() {
+    this.technicians.set(this.technicianService.getTechnicians());  
+  }
+
+  openRegisterModal() {
+    this.isEditing = false;
+    this.originalUsername = null;
+    this.technicianForm.reset();
+    this.modalVisible = true;
+  }
+
+  openEditModal(technician: any) {
+    console.log("entra a editar")
+    console.log(technician)
+    this.isEditing = true;
+    this.originalUsername = technician.username;
+    this.technicianForm.setValue({
+      username: technician.username,
+      email: technician.email,
+      role: technician.role
+    });
+    this.modalVisible = true;
+  }
+
+  saveTechnician() {
+    if (this.technicianForm.invalid) return;
+
+    const technicianData = this.technicianForm.value;
+    if (this.isEditing) {
+      console.log('Actualizando técnico:', technicianData);
+      this.technicianService.saveTechnician(technicianData,this.originalUsername); // Guardar técnico
+    } else {
+      console.log('Registrando técnico:', technicianData);
+      this.technicianService.saveTechnician(technicianData); // Guardar técnico
+    }
+    this.loadTechnicians();  // Recargar la lista de técnicos
+    this.closeModal();
+  }
+
+  deleteTechnician(username: string) {
+    this.technicianService.deleteTechnician(username); // Eliminar técnico
+    this.loadTechnicians();  // Recargar la lista de técnicos después de eliminar
+  }
+
+  closeModal() {
+    this.modalVisible = false;
+    this.technicianForm.reset();
+    this.originalUsername = null;  
+
+  }
 }
---
+++ iron-fix-app/src/app/shared/services/technician.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { User } from '../interfaces/user.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TechnicianService {
+
+  constructor() { }
+
+  // Obtener todos los técnicos desde localStorage
+  getTechnicians(): User[] {
+    const storage = JSON.parse(localStorage.getItem('appData') || '{"users":[],"equipment":[],"orders":[]}');
+    return storage.users.filter((user: any) => user.role === 'technician');
+  }
+
+  // Agregar o actualizar técnico
+  saveTechnician(technician:any, originalUsername?: string | null) {
+    const storage = JSON.parse(localStorage.getItem('appData') || '{"users":[],"equipment":[],"orders":[]}');
+
+    const usernameToSearch = originalUsername || technician.username;
+
+    const existingTechnician = storage.users.find((u: User) => u.username === usernameToSearch);
+    
+    if (existingTechnician) {
+      existingTechnician.username = technician.username;
+      existingTechnician.email = technician.email;
+      existingTechnician.role = technician.role;
+    } else {
+      storage.users.push(technician);
+    }
+
+    localStorage.setItem('appData', JSON.stringify(storage));
+  }
+
+  // Eliminar técnico
+  deleteTechnician(username: string) {
+    const storage = JSON.parse(localStorage.getItem('appData') || '{"users":[],"equipment":[],"orders":[]}');
+    storage.users = storage.users.filter((user: User) => user.username !== username);
+    localStorage.setItem('appData', JSON.stringify(storage));
+  }
+}
